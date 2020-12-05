48e2a6935d05c3c981a475e7056ded0cb7a98dde
Galya Bolganova <70851177+Galllya@users.noreply.github.com>
2020-12-05 18:32:00 +0700

    javascript

---
+++ script.js
@@ -0,0 +1,21 @@
+function openForm() {
+  document.getElementById("text_1").style.visibility = "visible"; }
+function openForm_2() {
+  document.getElementById("text_2").style.visibility = "visible"; }
+function openForm_3() {
+  document.getElementById("text_3").style.visibility = "visible"; }
+
+
+
+  const b = document.getElementById('text_1')
+  b.addEventListener('click', function () {
+      document.getElementById('text_1').style.visibility = 'hidden';})
+
+  const a = document.getElementById('text_2')
+  a.addEventListener('click', function () {
+      document.getElementById('text_2').style.visibility = 'hidden';})
+
+ const c = document.getElementById('text_3')
+  c.addEventListener('click', function () {
+      document.getElementById('text_3').style.visibility = 'hidden';})
+
